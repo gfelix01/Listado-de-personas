e7254e2c24e67177c8858cc6a3c991da14da3abe
Gabriel Arturo Felix Paez <gabriel_arturo01@hotmail.com>
2023-12-09 19:12:53 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+const personas = [
+
+  new Persona('Juan', 'Perez'),
+  new Persona('Luis', 'Melendez'),
+  new Persona('Maria', 'Rodriguez'),
+  new Persona('Jorge', 'Garcia'),
+  new Persona('Juan', 'Felix'),
+  new Persona('Pedro', 'Paez'),
+  new Persona('Alejandra', 'Rodriguez'),
+  new Persona('Heidy', 'mamguevo')
+]
+
+function mostrarPersonas () {
+  let texto = ''
+  for (const persona of personas) {
+    console.log(persona)
+    texto += `<li>${persona.nombre} ${persona.apellido}</li>`
+  }
+  document.getElementById('personas').innerHTML = texto
+}
+function agregarPersona () {
+  const forma = document.forms.forma
+  const nombre = forma.nombre
+  const apellido = forma.apellido
+  if (nombre.value != "" && apellido.value != ""){
+  const persona = new Persona(nombre.value, apellido.value)
+  console.log(persona)
+  personas.push(persona)
+  mostrarPersonas()
+}
+else{
+  console.log('no hay informacion que  agregar');
+}
+}
